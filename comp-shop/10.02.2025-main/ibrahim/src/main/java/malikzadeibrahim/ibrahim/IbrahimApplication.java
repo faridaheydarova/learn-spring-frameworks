5e4094b57cc8d7f05c072099c9afc541d4604ffa
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2025-04-22 21:37:31 +0400

    war faylina cevirdik

--- comp-shop/10.02.2025-main/ibrahim/src/main/java/malikzadeibrahim/ibrahim/IbrahimApplication.java
+++ comp-shop/10.02.2025-main/ibrahim/src/main/java/malikzadeibrahim/ibrahim/IbrahimApplication.java
@@ -2,9 +2,10 @@ package malikzadeibrahim.ibrahim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IbrahimApplication {
+public class IbrahimApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IbrahimApplication.class, args);
